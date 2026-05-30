7db289d7b9525b129e9f42deb11fc04f11fe2268
DylanW007 <453480@bsd48.org>
2026-05-29 23:49:29 -0700

    Add edge class

--- edge.cpp
+++ edge.cpp
@@ -6,7 +6,37 @@
 #include "edge.h"
 using namespace std;
 
-placeholder(){
-    
-    
+edge::edge(vertex* v1, vertex* v2, int weight){
+    vertex1 = v1;
+    vertex2 = v2;
+    this->weight = weight;
+}
+
+edge::~edge(){
+    // Destructor
+}
+
+vertex* edge::getVertex1(){
+    return vertex1;
+}
+
+vertex* edge::getVertex2(){
+    return vertex2;
+}
+
+int edge::getWeight(){
+    return weight;
+}
+
+vertex* edge::getOtherVertex(vertex* v){
+    if(v == vertex1){
+        return vertex2;
+    } else if(v == vertex2){
+        return vertex1;
+    }
+    return nullptr; // Return nullptr if the vertex is not part of this edge
+}
+
+void edge::printEdge(){
+    cout << vertex1->getLabel() << " --(" << weight << ")--> " << vertex2->getLabel() << endl;
 }
--- edge.h
+++ edge.h
@@ -7,9 +7,21 @@ using namespace std;
 
 class edge{
   public:
-  
+    edge(vertex* v1, vertex* v2, int weight);
+    ~edge();
+
+    vertex* getVertex1();
+    vertex* getVertex2();
+    int getWeight();
+
+    vertex* getOtherVertex(vertex* v);
+
+    void printEdge();
+
   private:
-    
+    vertex* vertex1;
+    vertex* vertex2;
+    int weight;
 };
 
 #endif
